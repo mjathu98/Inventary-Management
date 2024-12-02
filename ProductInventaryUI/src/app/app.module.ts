dfed346b6b9d91bbb592aaefff5dcb614d79d66d
mjathu24 <jathuwork@gmail.com>
2024-12-02 22:23:05 +0530

    Create Components/footer

--- ProductInventaryUI/src/app/app.module.ts
+++ ProductInventaryUI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavComponent
+    TopNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
