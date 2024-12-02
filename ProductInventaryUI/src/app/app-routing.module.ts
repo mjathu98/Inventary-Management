c19affc8478b6d1b15ee8ecdda22e605e9dde974
mjathu24 <jathuwork@gmail.com>
2024-12-02 22:00:57 +0530

    Create Components/top-nav

--- ProductInventaryUI/src/app/app-routing.module.ts
+++ ProductInventaryUI/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component:TopNavComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
